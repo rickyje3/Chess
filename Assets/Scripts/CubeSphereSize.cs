5398599df689688929281393fdb21e4fc5e1a186
rickyje3 <122560145+rickyje3@users.noreply.github.com>
2024-09-08 17:59:39 -0400

    Lab 2 done

--- Assets/Scripts/CubeSphereSize.cs
+++ Assets/Scripts/CubeSphereSize.cs
@@ -4,6 +4,38 @@ using UnityEngine;
 
 public class CubeSphereSize : MonoBehaviour
 {
-    public float sphereSize;
-    public float cubeSize;
+    public CubeSphereSizeEditor csse;
+
+    public enum ShapeType
+    {
+        None,
+        Cube,
+        Sphere
+    }
+
+    public ShapeType shapeType; // Property to determine if the object is a cube or sphere
+    public float sphereSize = 1f;
+    public float cubeSize = 1f;
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeSize();
+        }
+    }
+
+    public void ChangeSize()
+    {
+        if (shapeType == ShapeType.Sphere)
+        {
+            // Change sphere siZE
+            transform.localScale = new Vector3(sphereSize, sphereSize, sphereSize);
+        }
+        else if (shapeType == ShapeType.Cube)
+        {
+            // Change cube size
+            transform.localScale = new Vector3(cubeSize, cubeSize, cubeSize);
+        }
+    }
 }
--- Assets/Scripts/CubesSphereSizeEditor.cs
+++ Assets/Scripts/CubesSphereSizeEditor.cs
@@ -1,59 +1,119 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
-using static Unity.Burst.Intrinsics.X86;
+using System.Linq;
 
 [CustomEditor(typeof(CubeSphereSize)), CanEditMultipleObjects]
-public class CubesSphereSizeEditor : Editor
+public class CubeSphereSizeEditor : Editor
 {
+    private bool areCubesEnabled = true;  // Track the enable/disable state for cubes
+    private bool areSpheresEnabled = true;  // Track the enable/disable state for spheres
+
     public override void OnInspectorGUI()
     {
+        // Draw the default inspector GUI
         DrawDefaultInspector();
+
+        // Get the target object (the CubeSphereSize script)
+        CubeSphereSize cubeSphereSize = (CubeSphereSize)target;
+
+        ChangeSizeWarning(cubeSphereSize);
 
         using (new EditorGUILayout.HorizontalScope())
         {
-
             if (GUILayout.Button("Select all cubes"))
             {
-                
-                var allCubeBehaviour = GameObject.FindObjectsOfType<CubeSphereSize>();
-                var allCubeGameObjects = allCubeBehaviour.Select(cube => cube.gameObject).ToArray();
-                Selection.objects = allCubeGameObjects;
-                
+                SelectAllObjectsOfType(CubeSphereSize.ShapeType.Cube);
             }
-
-            //EditorGUILayout.Space();
 
             if (GUILayout.Button("Select all spheres"))
             {
-                
-                var allSphereBehaviour = GameObject.FindObjectsOfType<CubeSphereSize>();
-                var allSphereGameObjects = allSphereBehaviour.Select(sphere => sphere.gameObject).ToArray();
-                Selection.objects = allSphereGameObjects;
+                SelectAllObjectsOfType(CubeSphereSize.ShapeType.Sphere);
             }
         }
 
+        // Clear selection button
+        if (GUILayout.Button("Clear selection"))
+        {
+            Selection.objects = new Object[0];
+        }
+
+        using (new EditorGUILayout.HorizontalScope())
+        {
+            // Set button color based on whether cubes are enabled or disabled
+            Color cubeButtonColor = areCubesEnabled ? Color.green : Color.red;
+            GUI.backgroundColor = cubeButtonColor;
+
             if (GUILayout.Button("Disable/Enable all cubes", GUILayout.Height(40)))
             {
-                
-                var allCubeBehaviour = GameObject.FindObjectsOfType<CubeSphereSize>();
-                var allCubeGameObjects = allCubeBehaviour.Select(cube => cube.gameObject).ToArray();
-                Selection.objects = allCubeGameObjects;
+                ToggleObjectsEnabledState(CubeSphereSize.ShapeType.Cube, areCubesEnabled);
+                areCubesEnabled = !areCubesEnabled;
             }
+
+            // Set button color based on whether spheres are enabled or disabled
+            Color sphereButtonColor = areSpheresEnabled ? Color.green : Color.red;
+            GUI.backgroundColor = sphereButtonColor;
 
             if (GUILayout.Button("Disable/Enable all spheres", GUILayout.Height(40)))
             {
-                
-                var allSphereBehaviour = GameObject.FindObjectsOfType<CubeSphereSize>();
-                var allSphereGameObjects = allSphereBehaviour.Select(sphere => sphere.gameObject).ToArray();
-                Selection.objects = allSphereGameObjects;
-                
+                ToggleObjectsEnabledState(CubeSphereSize.ShapeType.Sphere, areSpheresEnabled);
+                areSpheresEnabled = !areSpheresEnabled;
             }
-        
+        }
+    }
+
+    // Method to display warnings based on the current size settings
+    public void ChangeSizeWarning(CubeSphereSize cubeSphereSize)
+    {
+        // Check sphere size for warnings
+        if (cubeSphereSize.shapeType == CubeSphereSize.ShapeType.Sphere)
+        {
+            if (cubeSphereSize.sphereSize > 3f)
+            {
+                EditorGUILayout.HelpBox("Sphere size cannot be greater than 3", MessageType.Warning);
+            }
+            else if (cubeSphereSize.sphereSize <= 0f)
+            {
+                EditorGUILayout.HelpBox("Sphere size cannot be less than or equal to 0", MessageType.Warning);
+            }
+        }
+
+        // Check cube size for warnings
+        if (cubeSphereSize.shapeType == CubeSphereSize.ShapeType.Cube)
+        {
+            if (cubeSphereSize.cubeSize > 3f)
+            {
+                EditorGUILayout.HelpBox("Cube size cannot be greater than 3", MessageType.Warning);
+            }
+            else if (cubeSphereSize.cubeSize <= 0f)
+            {
+                EditorGUILayout.HelpBox("Cube size cannot be less than or equal to 0", MessageType.Warning);
+            }
+        }
+    }
+
+    private void SelectAllObjectsOfType(CubeSphereSize.ShapeType shapeType)
+    {
+        var allObjects = GameObject.FindObjectsOfType<CubeSphereSize>();
+        var filteredObjects = allObjects
+            .Where(obj => obj.shapeType == shapeType)
+            .Select(obj => obj.gameObject)
+            .ToArray();
+        Selection.objects = filteredObjects;
+    }
+
+    private void ToggleObjectsEnabledState(CubeSphereSize.ShapeType shapeType, bool areCurrentlyEnabled)
+    {
+        // Use Resources.findobjectsoftypeall to find active and inactive objects
+        var allObjects = Resources.FindObjectsOfTypeAll<CubeSphereSize>();
+
+        foreach (var obj in allObjects)
+        {
+            if (obj.shapeType == shapeType)
+            {
+                obj.gameObject.SetActive(!areCurrentlyEnabled); // Toggle active state based on current state
+            }
+        }
     }
 }
+
 
