9fa9e45f7e9c87c4164455d987eb2f07686f452b
rickyje3 <122560145+rickyje3@users.noreply.github.com>
2024-10-02 16:22:45 -0400

    Empty the branch

--- Assets/Scripts/CubeSphereSize.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CubeSphereSize : MonoBehaviour
-{
-    public float sphereSize;
-    public float cubeSize;
-}
